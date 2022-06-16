ad0ce41b89aa821f68315266b038342ce659d94c
Romero Emmanuel (2) <emmanuel.romero.2@volvo.com>
2022-06-16 14:49:23 -0500

    modified arch to upload open tags

--- mainArch.py
+++ mainArch.py
@@ -1,15 +1,16 @@
-from functions import shipped_lines_query_dp, crown_hlg_kits_dp, des_opc
+from functions import shipped_lines_query_dp, crown_hlg_kits_dp, des_opc, open_tags
 import pandas as pd
 
+open_tags()
 
-archive_shipped_kits = pd.read_excel(r'C:\Users\A381375\Downloads\3PL OB Archive Data BYH 2022.xlsx', sheet_name='Shipped Kits')
-shipped_lines_query_dp(set_report=archive_shipped_kits)
-
-archived_dp_kits = pd.read_excel(r'C:\Users\A381375\Downloads\3PL OB Archive Data BYH 2022.xlsx', sheet_name='DP Kits')
-crown_hlg_kits_dp(set_report=archived_dp_kits)
-
-archived_description = pd.read_excel(r'C:\Users\A381375\OneDrive - Volvo Group\Onur\3pl-pipelines\opc_des.xlsx')
-des_opc(archived_description)
+# archive_shipped_kits = pd.read_excel(r'C:\Users\A381375\Downloads\3PL OB Archive Data BYH 2022.xlsx', sheet_name='Shipped Kits')
+# shipped_lines_query_dp(set_report=archive_shipped_kits)
+#
+# archived_dp_kits = pd.read_excel(r'C:\Users\A381375\Downloads\3PL OB Archive Data BYH 2022.xlsx', sheet_name='DP Kits')
+# crown_hlg_kits_dp(set_report=archived_dp_kits)
+#
+# archived_description = pd.read_excel(r'C:\Users\A381375\OneDrive - Volvo Group\Onur\3pl-pipelines\opc_des.xlsx')
+# des_opc(archived_description)
 
 
 
