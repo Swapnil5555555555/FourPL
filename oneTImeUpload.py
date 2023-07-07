d3ba122f2746b554cc9dbc4654bf74b8c9b6197c
Romero Emmanuel (2) <emmanuel.romero.2@volvo.com>
2023-07-07 11:24:41 -0500

    attempts to upload even after connection fails

--- oneTImeUpload.py
+++ oneTImeUpload.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import queries
 import connection
+import json
 from uuid import uuid4
 from datetime import datetime, timedelta
 import warnings
@@ -58,7 +59,14 @@ def is_none(val):
         return None
     else:
         return val
-
+    
+    
+def log_missed_record(record):
+    with open('readme.txt', 'w') as outfile:
+        record_json = record.to_json()
+        json.dump(record_json, outfile)
+        
+        
 def crown_lipert_productivity():
     
     global azure_cnxn, azure_cursor, oracle_cnxn
@@ -66,21 +74,32 @@ def crown_lipert_productivity():
     
     oracle_sql_query = CRN_LIP_PRODUCTIVITY
     productivity_df = pd.read_sql(oracle_sql_query, oracle_cnxn)
-
-    for index, row in productivity_df.iterrows():
-        # if set_report is None:
-            # try:
-        try:
-            azure_cursor.execute(INSERT_CRN_LIP_PRODUCTIVITY,
+    
+    def upload(row):
+        azure_cursor.execute(INSERT_CRN_LIP_PRODUCTIVITY,
                                     is_none(row['TYPE']),
                                     is_none(row['SKU_ID']),
                                     is_none(row['TAG_ID']),
                                     is_none(row['DSTAMP']),
                                     is_none(row['QTY']))
-            azure_cnxn.commit()
+        azure_cnxn.commit()
+        
+    print(productivity_df.shape)
+    for index, row in productivity_df.iterrows():
+        try:
+            upload(row)
         except Exception as error:
             print(error)
-            # TODO: Send an email that a record failed. Log the record
+            try:
+                azure_cnxn.close()
+            except:
+                print('unable to close connection')
+             
+            azure_cnxn = connection.azure_connection()
+            azure_cursor = azure_cnxn.cursor()
+            upload(row)
+            
+    
             
             
 
