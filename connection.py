127dc80d3c6f287d97365794dc4dbf4fad874883
Group ID A339580 <group.id.a339580@volvo.com>
2023-07-11 16:53:26 -0500

    re-name env variables

--- connection.py
+++ connection.py
@@ -8,9 +8,9 @@ cx_Oracle.init_oracle_client(config_dir=os.environ.get('ORACLE_NETWORK_CONFIG'))
 
 
 def azure_connection():
-    username = os.environ.get('_AZURE_USERNAME')
-    password = os.environ.get('_AZURE_PASSWORD')
-    database = os.environ.get('_AZURE_DATABASE')
+    username = os.environ.get('AZURE_USERNAME')
+    password = os.environ.get('AZURE_PASSWORD')
+    database = os.environ.get('AZURE_DATABASE')
     server = os.environ.get('AZURE_SERVER')
     driver = os.environ.get('AZURE_DRIVER')
     return pyodbc.connect(f'Driver={driver};Server={server};Database={database};Uid={username};Pwd={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
